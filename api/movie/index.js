6c9a56e3720146031f397e0d0a34304a2c97b3d0
young-yeon <as608683@naver.com>
2020-06-24 10:20:46 +0900

    2020-06-24

--- api/movie/index.js
+++ api/movie/index.js
@@ -2,10 +2,13 @@ const express = require("express")
 const router = express.Router()
 const ctrl = require("./movie.ctrl")
 
-router.get("/", ctrl.list) // 목록조회
-router.get("/:id", ctrl.checkId, ctrl.detail) // 상세조회
-router.post("/", ctrl.create) // 등록
-router.put("/:id", ctrl.checkId, ctrl.update) // 수정
-router.delete("/:id", ctrl.checkId, ctrl.remove) // 삭제
+router.get("/", ctrl.list); // 목록조회
+router.get("/new", ctrl.showCreatePage);
+router.get("/:id", ctrl.checkId, ctrl.detail); // 상세조회
+router.get("/:id/edit", ctrl.checkId, ctrl.showUpdatePage);
+
+router.post("/", ctrl.create); // 등록
+router.put("/:id", ctrl.checkId, ctrl.update); // 수정
+router.delete("/:id", ctrl.checkId, ctrl.remove); // 삭제
 
 module.exports = router
--- api/movie/movie.ctrl.js
+++ api/movie/movie.ctrl.js
@@ -1,76 +1,102 @@
-const MusicModel = require("../../models/movie")
-const mongoose = require("mongoose")
+const MovieModel = require("../../models/movie");
+const mongoose = require("mongoose");
 
 // id 유효성 체크
 const checkId = (req, res, next) => {
-    const id = req.params.id
-    
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).end()
-    }
+  const id = req.params.id;
 
-    next()
-}
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).end();
+  }
+
+  next();
+};
 
 // 목록 조회 (/api/music?limit=XX)
 const list = (req, res) => {
-    const limit = parseInt(req.query.limit || 10, 10)
-    
-    if (Number.isNaN(limit)) return res.status(400).end()
+  const limit = parseInt(req.query.limit || 10, 10);
 
-    MusicModel.find((err, result) => {
-        if (err) throw err
-        res.json(result)
-    }).limit(limit)
-}
+  if (Number.isNaN(limit)) return res.status(400).end();
+
+  MovieModel.find((err, result) => {
+    if (err) throw err;
+    res.render("movie/list", { result });
+  })
+    .limit(limit)
+    .sort({ _id: -1 });
+};
 
 // 상세 조회 (/api/music/:id)
 const detail = (req, res) => {
-    const id = req.params.id
-    
+  const id = req.params.id;
 
-    MusicModel.findById(id, (err, result) => {
-        if (err) return res.status(500).end()
-        if (!result) return res.status(404).end()
-        res.json(result)
-    })
-    
-  }
+  MovieModel.findById(id, (err, result) => {
+    if (err) return res.status(500).end();
+    if (!result) return res.status(404).end();
+    res.render("movie/detail", { result });
+  });
+};
 
 // 등록
 const create = (req, res) => {
-    const { title, director, year } = req.body;
-    if (!director || !title || !year) return res.status(400).end();
+  const { title, director, year } = req.body;
+  if (!director || !title || !year) return res.status(400).end();
 
-    MusicModel.create({ title, director, year }, (err, result) => {
-        if (err) throw err
-        res.status(201).json(result)
-    })
-
-}
+  MovieModel.create({ title, director, year }, (err, result) => {
+    if (err) throw err;
+    res.status(201).json(result);
+  });
+};
 
 // 수정 (/api/music/:id)
 const update = (req, res) => {
-    const id = req.params.id
+  const id = req.params.id;
 
-    const { title, director, year } = req.body;
-    MusicModel.findByIdAndUpdate(id, { title, director, year }, { new: true }, (err, result) => {
-        if (err) return res.status(500).end()
-        if (!result) return res.status(404).end()
-        res.json(result)
-    })
-
-}
+  const { title, director, year } = req.body;
+  MovieModel.findByIdAndUpdate(
+    id,
+    { title, director, year },
+    { new: true },
+    (err, result) => {
+      if (err) return res.status(500).end();
+      if (!result) return res.status(404).end();
+      res.json(result);
+    }
+  );
+};
 
 // 삭제 (/api/music/:id)
 const remove = (req, res) => {
-    const id = req.params.id
+  const id = req.params.id;
 
-    MusicModel.findByIdAndDelete(id, (err, result) => {
-        if (err) return res.status(500).end()
-        if (!result) return res.status(404).end()
-        res.json(result)
-    })
-}
+  MovieModel.findByIdAndDelete(id, (err, result) => {
+    if (err) return res.status(500).end();
+    if (!result) return res.status(404).end();
+    res.json(result);
+  });
+};
 
-module.exports = { checkId, list, detail, create, update, remove }
+const showCreatePage = (req, res) => {
+  res.render("movie/create");
+};
+
+const showUpdatePage = (req, res) => {
+  const id = req.params.id;
+
+  MovieModel.findById(id, (err, result) => {
+    if (err) return res.status(500).send("조회 에러");
+    if (!result) return res.status(404).send("해당 정보 없음");
+    res.render("movie/update", { result });
+  });
+};
+
+module.exports = {
+  checkId,
+  list,
+  detail,
+  create,
+  update,
+  remove,
+  showCreatePage,
+  showUpdatePage,
+};
